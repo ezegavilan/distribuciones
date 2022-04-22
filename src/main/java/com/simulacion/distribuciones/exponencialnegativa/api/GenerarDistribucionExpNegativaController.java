f60d980eaecc9c71f48d5ffecf4eeb22417b02ce
ezegavilan <ezegavilan.developer@gmail.com>
2022-04-22 12:21:58 -0300

    cross origin added

--- src/main/java/com/simulacion/distribuciones/exponencialnegativa/api/GenerarDistribucionExpNegativaController.java
+++ src/main/java/com/simulacion/distribuciones/exponencialnegativa/api/GenerarDistribucionExpNegativaController.java
@@ -6,16 +6,14 @@ import com.simulacion.distribuciones.shared.model.GeneradorResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class GenerarDistribucionExpNegativaController {
     private final GenerarDistribucionExpNegativaUseCase generarDistribucionExpNegativaUseCase;
 
--- src/main/java/com/simulacion/distribuciones/uniformeab/api/GetDistribucionUniformeABController.java
+++ src/main/java/com/simulacion/distribuciones/uniformeab/api/GetDistribucionUniformeABController.java
@@ -6,16 +6,14 @@ import com.simulacion.distribuciones.uniformeab.service.in.GenerarDistribucionUn
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 public class GetDistribucionUniformeABController {
     private final GenerarDistribucionUniformeABUseCase generarDistribucionUniformeABUseCase;
 
